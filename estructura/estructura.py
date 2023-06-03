2f606ace63e2f6f8ea970ee2effda988581b5a88
Jose Miguel Millan <jevo160296@gmail.com>
2023-06-03 09:32:25 -0500

    Add redirect button

--- estructura/estructura.py
+++ estructura/estructura.py
@@ -1,8 +1,29 @@
 import streamlit as st
+from pagina_principal import main
 
 
 def trim_line_jumps(text: str):
     return '\n'.join(text.split('\n')[1:-1])
+
+
+def redirect_button(url: str, text: str = None, color="#FD504D"):
+    st.markdown(
+        f"""
+    <a href="{url}" target="_self">
+        <div style="
+            display: inline-block;
+            padding: 0.5em 1em;
+            margin: 1em 0em 1em 0em;
+            color: #FFFFFF;
+            background-color: {color};
+            border-radius: 3px;
+            text-decoration: none;">
+            {text}
+        </div>
+    </a>
+    """,
+        unsafe_allow_html=True
+    )
 
 
 class Articulo:
@@ -11,6 +32,12 @@ class Articulo:
         self.markdown = markdown
 
     def render(self):
+        main_page_link = 'Conoce_nuestros_servicios'
+
         st.title(self.titulo)
 
+        redirect_button(main_page_link, "Conoce nuestros servicios")
+
         st.markdown(self.markdown)
+
+        redirect_button(main_page_link, "Conoce nuestros servicios")
